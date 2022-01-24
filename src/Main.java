3f1858872a6ddba1a20135ae72e120f518c1aa72
piedrohammer <piiedrohammer@gmail.com>
2022-01-24 12:37:50 -0300

    Final Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -15,14 +14,57 @@ public class Main {
         curso2.setDescricao("descrição curso js");
         curso2.setCargaHoraria(4);
 
+
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("mentoria java");
         mentoria.setDescricao("descrição mentoria java");
         mentoria.setDate(LocalDate.now());
 
 
-        System.out.println(curso1);
+       /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devPiedro = new Dev();
+        devPiedro.setNome("Piedro");
+        devPiedro.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Piedro: " + devPiedro.getConteudosInscritos());
+
+        devPiedro.progredir();
+        devPiedro.progredir();
+
+        System.out.println("=");
+        System.out.println("Conteúdos Inscritos Piedro: " + devPiedro.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Piedro: " + devPiedro.getConteudosConcluidos());
+        System.out.println("XP: " + devPiedro.calcularTotalXp());
+
+        System.out.println("===========");
+
+        Dev devPietra = new Dev();
+        devPietra.setNome("Pietra");
+        devPietra.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Pietra: " + devPietra.getConteudosInscritos());
+
+        devPietra.progredir();
+        devPietra.progredir();
+        devPietra.progredir();
+        System.out.println("=");
+        System.out.println("Conteúdos Inscritos Pietra: " + devPietra.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Pietra: " + devPietra.getConteudosConcluidos());
+        System.out.println("XP :" + devPietra.calcularTotalXp());
+
+
+
+
+
+
+
     }
 }
